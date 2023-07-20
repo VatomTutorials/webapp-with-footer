d5809b3e2deb2d8df32fad0985e638cb72f66c38
Joe Sandmeyer <github.com@unconscious.com>
2023-07-20 08:13:31 -0400

    experiment

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Footer } from './components/Footer';
 import HomePage from './pages/Home';
 import AboutPage from './pages/About';
 
 function App() {
   return (
     <BrowserRouter basename={"/" }>
+    	<div>
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/home" element={<HomePage />}></Route>
           <Route path="/about" element={<AboutPage />}></Route>
         </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
 
 export default App
+
+
+// <Footer />
+// <p> footer </p>
